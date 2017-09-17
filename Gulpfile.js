41ce2a24d7cef8986fa5679aa4837000c1c630d3
KranthikumarS <kranthikumarsha@gmail.com>
2017-09-17 23:46:35 +0530

    First develop setup

---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+// *************************************
+//
+//   Gulpfile
+//
+// *************************************
+//
+// Available tasks:
+//   'gulp'
+//   'gulp build'
+//
+// *************************************
+
+
+// -------------------------------------
+//   Modules
+// -------------------------------------
+//
+// gulp                       : The streaming build system
+// run-sequence               : Organize gulp tasks
+// require-dir                : Organize gulp tasks in a sub-directory
+//
+// -------------------------------------
+
+
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
+require('require-dir')('./gulp');
+
+
+// -------------------------------------
+//   Task: Build
+// -------------------------------------
+
+gulp.task('build', function () {
+  runSequence(
+    // 'modernizr',
+    'js',
+    'sass',
+    'copy',
+    'partials',
+    'webserver',
+    'watch'
+  );
+});
+
+// -------------------------------------
+//   Task: Default
+// -------------------------------------
+gulp.task('default', ['build']);
+
+// gulp.task('watch', ["build"], function() {
+//     return gulp.start(['webserver']);
+// });
